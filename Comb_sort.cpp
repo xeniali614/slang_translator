8f54bb2520a81d84a0f03232da31159895d74415
Xenia Li <188245409+xeniali614@users.noreply.github.com>
2025-10-31 14:35:43 -0400

    Comb Sort first version (Xenia Li)

---
+++ Comb_sort.cpp
@@ -0,0 +1,66 @@
+//
+// Created by xenia on 10/31/2025.
+//
+
+#include "Comb_sort.h"
+
+
+void combsort_alphabet(vector<pair<string,string>>& alphabet){
+    int gap = alphabet.size();
+    float shrink= 1.3;
+    bool sorted = false;
+
+    while(!sorted){
+        gap = int(gap/shrink);
+        if(gap <= 1){
+            gap=1;
+            sorted = true;
+        }
+        for(int i=0;i+gap <alphabet.size();i++){
+            if(alphabet[i].first > alphabet[i+gap].first){
+                swap(alphabet[i],alphabet[i+gap]);
+                sorted = false;
+            }
+        }
+    }
+}
+
+void combsort_upvotes(vector<tuple<int,string,string>>& alphabet){
+    int gap = alphabet.size();
+    float shrink= 1.3;
+    bool sorted = false;
+
+    while(!sorted){
+        gap = int(gap/shrink);
+        if(gap <= 1){
+            gap=1;
+            sorted = true;
+        }
+        for(int i=0;i+gap <alphabet.size();i++){
+            if(get<0>(alphabet[i]) > get<0>(alphabet[i+gap])){
+                swap(alphabet[i],alphabet[i+gap]);
+                sorted = false;
+            }
+        }
+    }
+}
+
+void combsort_downvotes(vector<tuple<int,string,string>>& alphabet){
+    int gap = alphabet.size();
+    float shrink= 1.3;
+    bool sorted = false;
+
+    while(!sorted){
+        gap = int(gap/shrink);
+        if(gap <= 1){
+            gap=1;
+            sorted = true;
+        }
+        for(int i=0;i+gap <alphabet.size();i++){
+            if(get<0>(alphabet[i]) > get<0>(alphabet[i+gap])){
+                swap(alphabet[i],alphabet[i+gap]);
+                sorted = false;
+            }
+        }
+    }
+}
---
+++ Comb_sort.h
@@ -0,0 +1,20 @@
+//
+// Created by xenia on 10/31/2025.
+//
+
+#ifndef COMB_SORT_H
+#define COMB_SORT_H
+
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <map>
+using namespace std;
+
+void combsort_alphabet(std::vector<std::string>& alphabet);
+void combsort_upvotes(vector<tuple<int,string,string>>& alphabet);
+void combsort_downvotes(vector<tuple<int,string,string>>& alphabet);
+
+#endif //COMB_SORT_H
