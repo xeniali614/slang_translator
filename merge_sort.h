33aa84d33ae2bf56840bf7e14faf4984305e1b64
Xenia Li <188245409+xeniali614@users.noreply.github.com>
2025-11-02 18:33:50 -0500

    Fixed <> in merge-sort.h

--- merge_sort.h
+++ merge_sort.h
@@ -14,5 +14,5 @@
 void mergeAlphaData(std::vector<std::string>& slang, int low, int mid, int high);
 void mergeAlphaSort(std::vector<std::string>& slang, int low, int high);
 void mergeVoteData(std::vector<std::tuple<int, std::string, std::string>>& slang, int low, int mid, int high);
-void mergeVoteSort(std::vector<<std::tuple<int, std::string, std::string>>>& slang, int low, int high);
+void mergeVoteSort(std::vector<std::tuple<int, std::string, std::string>>& slang, int low, int high);
 #endif //MERGE_SORT_H
